995f6a1cd67c159f6ac7dbb1f821ba704a001307
nagol68 <lmccarthy98@outlook.com>
2021-04-06 11:53:31 -0500

    Create AddToTeams.ps1
    Added Teams Script

---
+++ AddToTeams.ps1
@@ -0,0 +1,17 @@
+﻿#
+# !!!Must call Connect-MicrosoftTeams before running!!!
+#
+# Paste entire list of Teams between single set of quotes
+#
+
+$TeamsToAdd = "TEAMS"
+
+$User = "EMAIL"
+
+$TeamsToAdd = $TeamsToAdd.Split([string[]]"`r`n", [StringSplitOptions]::None)
+
+$TeamsIDs = ForEach ($Team in $TeamsToAdd) {
+    $TeamID = Get-Team -DisplayName $Team | Where {$_.DisplayName -match "$Team$"} | Select -expand GroupID
+    Add-TeamUser -GroupId $TeamID -User $User
+}
+
