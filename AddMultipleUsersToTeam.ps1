be572381c4848033e207bdc886747bb39e1305ad
nagol68 <lmccarthy98@outlook.com>
2021-05-17 09:29:52 -0500

    Create AddMultipleUsersToTeam.ps1

---
+++ AddMultipleUsersToTeam.ps1
@@ -0,0 +1,18 @@
+﻿#
+# !!!Must call Connect-MicrosoftTeams before running!!!
+#
+# Paste entire list of users between single set of quotes
+#
+
+$TeamToAdd = "TEAM"
+
+$Users = "USERS"
+
+$Users = $Users.Split([string[]]"`r`n", [StringSplitOptions]::None)
+
+$TeamID = Get-Team -DisplayName $TeamToAdd | Where {$_.DisplayName -match "$TeamToAdd$"} | Select -expand GroupID
+
+ForEach ($User in $Users) {
+    $User = $User + "@supportworks.com"
+    Add-TeamUser -GroupId $TeamID -User $User
+}
