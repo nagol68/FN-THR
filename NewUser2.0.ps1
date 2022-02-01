d446914fede52196ea3b33c57f6a8e68687bdea6
nagol68 <lmccarthy98@outlook.com>
2022-02-01 16:56:04 -0600

    Update NewUser2.0.ps1
    -80% complete with copy new user
    -Logic setup for net new user
    -Currently designed for Thrasher only. Must add switch for SW

--- NewUser2.0.ps1
+++ NewUser2.0.ps1
@@ -7,34 +7,40 @@
 ###
 #
 
-function Show-Menu
-{
-    param (
-        [string]$Title = 'Create New User'
-    )
-    Write-Host "`n================ $Title ================"
-    
-    Write-Host "Select user type"
-    Write-Host "`n"
-    Write-Host "`n1. Microsoft 365 Business Basic    | Office users with a standalone Microsoft Office licenses (RARE), sales, or service members."
-    Write-Host "2. Microsoft 365 Business Standard | Office users with a new computer or inheriting a newer PC"
-    Write-Host "3. Exchange Online (Plan 1)        | Utility accounts, service accounts, etc."
-    Write-Host "4. Office 365 F3                   | Crew members. (Exchange and Teams only) (do not add Azure license to Installers)"
-}
+#
+# Menu Formats
+#
 
-function First-Menu
+function Command-Menu
 {
     
-    Write-Host "First user setup within the hour?"
+    Write-Host "`nDo services need to be reconnected?"
     Write-Host "`n"
     Write-Host "`n1. Yes."
     Write-Host "2. No."
 }
 
+function Copy-User-Menu
+{
+    
+    Write-Host "`nIs there a user that can be copied?"
+    Write-Host "`n"
+    Write-Host "`n1. Yes."
+    Write-Host "2. No."
+}
+
+function Company-Menu
+{
+	Write-Host "`nWhich company is the hire for?"
+    Write-Host "`n"
+    Write-Host "`n1. Thrasher."
+    Write-Host "2. Supportworks."
+}
+
 function THR-Location-Menu
 {
     
-    Write-Host "Select Location"
+    Write-Host "`nSelect Location"
     Write-Host "`n"
     Write-Host "`n1. Des Moines."
     Write-Host "2. Grand Island."
@@ -48,31 +54,71 @@ function THR-Location-Menu
 function THR-Department-Menu
 {
     
-    Write-Host "Select Department"
+    Write-Host "`nSelect Department"
     Write-Host "`n"
     Write-Host "`n1. Production."
     Write-Host "2. Service."
 	Write-Host "3. Sales."
-	Write-Host "4. Other."
+	Write-Host "4. Office."
+	Write-Host "5. Other."
+}
+
+function THR-Office-Menu
+{
+    
+    Write-Host "`nSelect an Office Department"
+    Write-Host "`n"
+    Write-Host "`n1. Customer Care - Production."
+    Write-Host "2. Customer Care - Sales & Service."
+	Write-Host "3. Executive."
+	Write-Host "4. Finance."
+	Write-Host "5. HR."
+	Write-Host "6. Legal."
+	Write-Host "7. Marketing."
+	Write-Host "8. Other."
 }
 
 function THR-Production-Menu
 {
     
-    Write-Host "Select Job Title"
+    Write-Host "`nSelect Job Title"
     Write-Host "`n"
     Write-Host "`n1. Installer."
     Write-Host "2. Lead Installer."
-	Write-Host "4. Other."
+	Write-Host "3. Team Leader."
+	Write-Host "4. Sr. Team Leader."
+	Write-Host "5. Other."
 }
 
-$LicenseSKU = ""
+function THR-Marketing-Menu
+{
+    
+    Write-Host "`nSelect Job Title"
+    Write-Host "`n"
+    Write-Host "`n1. Brand Ambassador."
+	Write-Host "2. Other."
+}
+
+function Title-Menu
+{
+	Write-Host "`nIs the above title correct for $fullName"
+    Write-Host "`n"
+    Write-Host "`n1. Yes."
+    Write-Host "2. No."
+}
+
+#
+# Variables
+# 
+$licenseSKU = ""
+$company = ""
+$location = ""
 
 #Start-ADSyncSyncCycle -PolicyType Delta
 
 Clear-host
 
-First-Menu
+Command-Menu
  $selection = Read-Host "Please make a selection"
  switch ($selection)
  {
@@ -87,166 +133,73 @@ First-Menu
 
 Clear-host
 
-Write-Host "`nAll fields must be filled out unless specified`n`nPaste each field directly from ticket"
+Write-Host "`nAll fields must be filled out unless specified`n`n--Paste each field directly from ticket--"
 
-$email = Read-Host "`nEnter email of new user"
-$username = $email -replace ("@(.*)","") #Remove @ and everything after
-$domain = $email -replace ("(.*)@","") #Remove @ and everything before
+$fullName = Read-Host "`nEnter full name of new user" #Gets full name from input
+$firstName = $fullName -replace (" (.*)","") #Remove everything after the space
+$lastName = $fullName -replace ("(.*) ","") #Remove everything before the space
+$SAMName = $firstName+"."+$lastName #Formats name into first.last
+#
+#    THIS NEEDS UPDATED TO WORK FOR SUPPORTWORKS
+#    DO THIS!!!!!!!!!!!!!!!!!!!!!!!!!
+#    IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!
+#
 
-$mobile = Read-Host "`nEnter mobile number"
-$mobile = $mobile -replace '\.','-'
 
-$rc = Read-Host "`nEnter RC number (if applicable)"
+$password = Read-Host "`nEnter a password for the new user" #Gets password from input
 
-if($domain -eq "gothrasher.com"){
-    THR-Location-Menu
-	 $selection = Read-Host "Please make a selection"
-	 switch ($selection)
-	 {
-		 '1' {
-			$location = "Midwest - Des Moines"
-			Write-Host "`nBe Sure to Adjust Domain to @midwestfr.com!`n"
-		} '2' {
-			$location = "Thrasher - Grand Island"
-		} '3' {
-			$location = "Thrasher - Kansas City"
-		} '4' {
-			$location = "Thrasher - Omaha"
-		} '5' {
-			$location = "Thrasher - Sioux City"	
-		} '6' {
-			$location = "Thrasher - Springfield"
-		} '7' {
-			$location = "Thrasher - Wichita"
-		}
-	}
-}elseif($domain -eq "midwestfr.com"){
-	$location = "Midwest - Des Moines"
-}elseif($domain -eq "hellogaragehq.com"){
-	$location = "Omaha HQ"
-}elseif($domain -eq "supportworks.com"){
-	$location = "Omaha HQ"
-}
 
-if($domain -eq "gothrasher.com"){
-    THR-Department-Menu
-	 $selection = Read-Host "Please make a selection"
-	 switch ($selection)
-	 {
-		 '1' {
-			$department = "Production"
-		} '2' {
-			$department = "Service"
-		} '3' {
-			$department = "Sales"
-		} '4' {
-			$department = Read-Host "`nEnter department"
-		}
-	}
-}else{
-	$department = Read-Host "`nEnter department"
-}
 
-if($department -eq "Production"){
-    THR-Production-Menu
-	 $selection = Read-Host "Please make a selection"
-	 switch ($selection)
-	 {
-		 '1' {
-			$job = "Installer"
-		} '2' {
-			$job = "Lead Installer"
-		} '3' {
-			$job = Read-Host "`nEnter job title"
-		}
-	}
-}elseif($department -eq "Service"){
-	$job = "Service Technician"
-}elseif($department -eq "Sales"){
-	$job = "System Design Specialist"
-}else{
-	$job = Read-Host "`nEnter job title"
-}
-
-$manager = Read-Host "`nEnter manager's username"
-
-$TeamsToAdd = Read-Host "`nEnter list of teams to be added (one per line)"
-$TeamsToAdd = $TeamsToAdd.Split([string[]]"`r`n", [StringSplitOptions]::None) #Split multi-lined string into list 
-
-$Thrasher = $TRUE
-
-Show-Menu –Title 'License Selection'
+Copy-User-Menu
  $selection = Read-Host "Please make a selection"
  switch ($selection)
  {
      '1' {
-         $LicenseSKU = "thrashercompanies:O365_BUSINESS_ESSENTIALS","thrashercompanies:RIGHTSMANAGEMENT"
+		#
+        # Copying User
+		#
+        $copiedUser = Read-Host "`nPlease enter the username of the user to be copied" #Gets user based on input
+		$copiedAttributes = Get-ADUser -Identity $copiedUser -Properties Office,Department,Manager,Title,Company #Gets attributes
+        $OU = ($copiedAttributes).DistinguishedName.split(",",2)[1] #Gets OU
+		
+        #Creating new user
+		New-ADUser `
+            -Name $fullName -GivenName $firstName -Surname $lastName -SAMAccountName $SAMName -DisplayName $fullName `
+			-Path $OU `
+			-Manager $copiedAttributes.Manager -Office $copiedAttributes.Office -Department $copiedAttributes.Department -Company $copiedAttributes.Company `
+			-AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -ChangePasswordAtLogon $false `
+			-Enabled $true
+
+        #Menu to check if title is the same as copied user
+        Write-Host "`nTitle: "+$copiedAttributes.Title
+        Title-Menu
+        $selection = Read-Host "Please make a selection"
+         switch ($selection)
+         {
+            '1' {
+                #Sets title the same as copied user
+                $title = $copiedAttributes.Title
+            } '2' {
+                #Sets new title based on user input
+                $title = Read-Host "`nPlease enter $fullName's title"
+            }
+        }
+
+        #Sets title
+        Set-ADUser -Identity $SAMName -Title $title 
+
+        #Copies membership
+        $copiedMembership = Get-ADUser $copiedUser -prop MemberOf
+        $newMembership = Get-ADUser $SAMName -prop MemberOf
+        $copiedMembership.MemberOf | Where{$newMembership.MemberOf -notcontains $_} |  Add-ADGroupMember -Member $newMembership
+
+		
      } '2' {
-         $LicenseSKU = "thrashercompanies:O365_BUSINESS_PREMIUM","thrashercompanies:RIGHTSMANAGEMENT"
-     } '3' {
-         $LicenseSKU = "thrashercompanies:EXCHANGESTANDARD"
-     } '4' {
-         $LicenseSKU = "thrashercompanies:DESKLESSPACK"
+		#
+        # Net-New User
+        #
+
+
+
      }
  }
- 
-Write-Host "`nPlease wait..."
-
-# Filling in AD attributes
-
-Set-ADUser -Identity $username -Description $job -Office $location -MobilePhone $mobile -Department $department -Title $job -Manager $manager -PasswordNeverExpires $true
-
-if($rc){
-	$rc = $rc -replace '\.','-'
-    Set-ADUser -Identity $username -OfficePhone $rc
-    }
-    else{
-    Set-ADUser -Identity $username -OfficePhone $mobile
-}
-
-if($domain -eq "gothrasher.com"){
-    Set-ADUser -Identity $username -Company "Thrasher, Inc."
-    }elseif($domain -eq "midwestfr.com"){
-    Set-ADUser -Identity $username -Company "Midwest Foundation Repair"
-    }elseif($domain -eq "supportworks.com"){
-    Set-ADUser -Identity $username -Company "Supportworks, Inc."
-}
-
-
-# Adding M365 License
-
-Set-Msoluser -UserPrincipalName $email -UsageLocation "US"
-Set-MsolUserLicense -UserPrincipalName $email -AddLicenses $LicenseSKU
-
-# Adding to Teams
-
-$TeamsID = ForEach ($Team in $TeamsToAdd) {
-    $TeamID = Get-Team -DisplayName $Team | Where {$_.DisplayName -match "$Team$"} | Select -expand GroupID
-    Add-TeamUser -GroupId $TeamID -User $email
-}
-
-# Mailbox Permissions
-
-if($Thrasher){
-    Set-MailboxFolderPermission `
-        ${email}:\Calendar `
-        -user Default `
-        -AccessRights Reviewer
-        
-    Add-MailboxFolderPermission `
-        ${email}:\Calendar `
-        -user ThrasherDefaultAuthor@gothrasher.com `
-        -AccessRights Reviewer
-}else{
-    Set-MailboxFolderPermission `
-        ${email}:\Calendar `
-        -user Default `
-        -AccessRights Reviewer
-        
-    Add-MailboxFolderPermission `
-        ${email}:\Calendar `
-        SupportworksDefaultAuthor@supportworks.com `
-        -AccessRights Author
-}
-
-Write-host "`nUser Setup is Complete!"
